d016e0dc5797cedea5d7875a1d7aa48b19c49c4e
julian <github2018@studio42.dk>
2018-08-09 11:32:50 +0200

    use express to serve static files

--- main.js
+++ main.js
@@ -1,35 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-const util_1 = require("util");
+const express = require("express");
 const path_1 = require("path");
-const url_1 = require("url");
-const readFile = util_1.promisify(fs.readFile);
+const wt = require('webtask-tools');
 const staticDir = "node_modules/dapp-form/docs/";
-const urlPath = "dappform-tasks-host";
-const mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"
-};
-module.exports = async function (context, req, res) {
-    const path = url_1.parse(req.url).pathname;
-    const file = (path === '/dappform-tasks-host') ? "index.html" : ""; // default file to serve in directories
-    const localPath = path.replace(urlPath, '') + file;
-    let fileContents;
-    try {
-        const file = path_1.join(__dirname, staticDir, localPath);
-        fileContents = await readFile(file);
-    }
-    catch (e) {
-        console.error(e);
-        res.writeHead(500, "Trouble");
-        return res.end();
-    }
-    const mimeType = mimeTypes[localPath.substr(localPath.lastIndexOf('.') + 1)] || "text/plain";
-    res.writeHead(fileContents ? 200 : 404, { 'Content-Type': mimeType });
-    res.end(fileContents);
-};
+const app = express();
+app.use(express.static(path_1.join(__dirname, staticDir)));
+module.exports = wt.fromExpress(app);
--- main.ts
+++ main.ts
@@ -1,39 +1,12 @@
-import * as fs from 'fs'
-import { promisify } from 'util'
-import { IncomingMessage } from 'http'
+import * as express from 'express'
 import { join } from 'path'
-import { parse } from 'url'
 
-const readFile = promisify(fs.readFile)
+const wt = require('webtask-tools')
+
 const staticDir = "node_modules/dapp-form/docs/"
-const urlPath = "dappform-tasks-host"
 
-const mimeTypes = {
-  "html": "text/html",
-  "jpeg": "image/jpeg",
-  "jpg": "image/jpeg",
-  "png": "image/png",
-  "js": "text/javascript",
-  "css": "text/css"
-}
+const app = express()
 
-module.exports = async function (context, req:IncomingMessage, res) {
-  const path = parse(req.url).pathname
-  const file = (path === '/dappform-tasks-host') ? "index.html" : "" // default file to serve in directories
-  const localPath = path.replace(urlPath, '') + file
-  let fileContents:Buffer
-  try {
-    const file = join( __dirname, staticDir, localPath)
-    fileContents = await readFile( file )
-  }
-  catch (e) {
-    console.error(e)
-    res.writeHead(500, "Trouble")
-    return res.end()
-  }
+app.use(express.static(join(__dirname, staticDir)))
 
-  const mimeType = mimeTypes[ localPath.substr( localPath.lastIndexOf('.')+1 )] || "text/plain"
-
-  res.writeHead(fileContents ? 200 : 404, { 'Content-Type': mimeType})
-  res.end(fileContents)
-}  
+module.exports = wt.fromExpress(app)
